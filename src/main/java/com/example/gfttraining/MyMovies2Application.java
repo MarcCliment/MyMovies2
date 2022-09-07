db98c50f551454971e56ab7df8f47162746251a0
Marc <mccp@gft.com>
2022-09-07 10:16:44 +0000

    Commit 1
    Initial commit

---
+++ src/main/java/com/example/gfttraining/MyMovies2Application.java
@@ -0,0 +1,13 @@
+package com.example.gfttraining;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyMovies2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyMovies2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/gfttraining/MyMovies2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gfttraining;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyMovies2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
